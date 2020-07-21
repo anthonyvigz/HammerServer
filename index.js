51421b517da5f8bddef2021db83fe1cb9790d510
anthonyvigz <anthonyvigz@gmail.com>
2020-07-21 16:28:41 -0400

    updated port

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log(`This is the Hammer Server running on port ${port}`);
+  console.log(`\n** Welcome to TouchBase! Running on port ${port} **\n`);
 });
